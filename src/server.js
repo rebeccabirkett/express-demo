97cf1a765f098b1fff97f858c0b5b40cdb87ac79
Rebecca Birkett <rebeccabirkett@outlook.com>
2021-03-29 16:48:34 +0100

    get and post methods added in

--- src/server.js
+++ src/server.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(`Hello, your name is ${req.query}`);
+})
+
+app.get("/data", (req, res) => {
+    res.send({ name: "Bex", age: 29});
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send({
+        message: `You entered your info under the name ${req.body.name}`,
+    });
+});
+
+// app.get("/person", (req, res) => {
+//     res.send({ message: "Showing all people:" });
+// });
+
+// app.get("/person/:id", (req, res) => {
+//     console.log(req.params.id);
+
+//     res.send({ message: "Success" });
+// });
+
+
 app.listen(5000, () => {
-    console.log("listening on port 5000")
+    console.log("listening on port 5000");
 });
